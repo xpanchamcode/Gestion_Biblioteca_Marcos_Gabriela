f1789128ce1b11d3a65bdc45d88e4814b4389d17
runnerbunny483 <runnerbunny483@gmail.com>
2024-10-16 13:14:07 +0200

    Commit Gabriela

--- src/main/java/BibliotecaMarcosGabriela/Main.java
+++ src/main/java/BibliotecaMarcosGabriela/Main.java
@@ -2,6 +2,10 @@ package BibliotecaMarcosGabriela;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hola!");
+        System.out.println("HOla marcos");
+        System.out.println("has");
     }
+
 }
