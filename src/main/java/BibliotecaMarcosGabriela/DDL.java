aaa0017170446d04a31dc55773dc4f4d6c72bef1
xpanchamcode <marcospjob.5@gmail.com>
2024-10-18 11:59:00 +0200

    Commit inicial.

--- src/main/java/BibliotecaMarcosGabriela/DDL.java
+++ src/main/java/BibliotecaMarcosGabriela/DDL.java
@@ -6,7 +6,7 @@ import java.sql.Statement;
 
 public class DDL {
     private static Connection conexion = JDBC.getConexion(); //Obtengo la conexión mediante la clase JDBC
-    private static String nombreBD = ""; //Establezco el nombre de la BD
+    private static String nombreBD = "Biblioteca"; //Establezco el nombre de la BD
 
     //Método para usar la BD:
     public static void useBD() throws SQLException {
