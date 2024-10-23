2f281dc96613e993374bb3b4d116a23bd977b129
xpanchamcode <marcospjob.5@gmail.com>
2024-10-23 12:28:29 +0200

    Commit Main

--- src/main/java/BibliotecaMarcosGabriela/Main.java
+++ src/main/java/BibliotecaMarcosGabriela/Main.java
@@ -1,11 +1,24 @@
 package BibliotecaMarcosGabriela;
 
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            DDL.useBD(); //Uso la BD para conectarla a mi programa
 
-        System.out.println("Hola!");
-        System.out.println("HOla marcos");
-        System.out.println("has");
+//          HACE FALTA INSTANCIAR ESTAS CLASES PARA INICIALIZAR LOS ARRAYLIST EN LOS CONSTRUCTORES
+            GestionLibros gestionLibros = new GestionLibros();
+            GestionAutores gestionAutores = new GestionAutores();
+            GestionUsuarios gestionUsuarios = new GestionUsuarios();
+            GestionPrestamos gestionPrestamos = new GestionPrestamos();
+            GestionLibroAutor gestionLibroAutor = new GestionLibroAutor();
+
+            //Instanciamos el menú, que lo abrirá directamente.
+            Menu menu = new Menu();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
-
 }
